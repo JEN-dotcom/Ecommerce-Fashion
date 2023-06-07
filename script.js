caa7364c2b027780096067707db0d5b3234df436
John Enyinwa <johnenyinwa@gmail.com>
2023-06-07 15:34:01 +0100

    added link to logo

--- script.js
+++ script.js
@@ -2,23 +2,6 @@ const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
 const MainImg = document.getElementById("MainImg");
-        const smallimg = document.getElementsByClassName("small-img");
-
-        smallimg[0].onclick = function() {
-            MainImg.src = smallimg[0].src;
-        }
-
-        smallimg[1].onclick = function() {
-            MainImg.src = smallimg[1].src;
-        }
-
-        smallimg[2].onclick = function() {
-            MainImg.src = smallimg[2].src;
-        }
-
-        smallimg[3].onclick = function() {
-            MainImg.src = smallimg[3].src;
-        }
 
 if (bar) {
     bar.addEventListener('click', () => {
@@ -30,5 +13,4 @@ if (close) {
     close.addEventListener('click', () => {
         nav.classList.remove('active');
     })
-}
-
+};
